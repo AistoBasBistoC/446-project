36d5585a4bf68041985ea9e76339d71f6e2f2abe
AistoBasBistoC <ahhitsscott@gmail.com>
2021-04-15 17:22:12 -0700

    added code for each state of the machine

--- Project A/Bin_7Segment.v
+++ Project A/Bin_7Segment.v
@@ -22,6 +22,22 @@
 
 module Bin_7Segment(
     input [3:0] Bin,
-    input [7:0] Seven_Segment
+    output reg [7:0] Seven_Segment
     );
+    
+    always@(*)
+    begin
+    case(Bin)
+    4'b0000: Seven_Segment = 8'hC0; //0
+    4'b0001: Seven_Segment = 8'hF9; //1
+    4'b0010: Seven_Segment = 8'hA4; //2
+    4'b0011: Seven_Segment = 8'hB0; //3
+    4'b0100: Seven_Segment = 8'h99; //4
+    4'b0101: Seven_Segment = 8'h92; //5
+    4'b0110: Seven_Segment = 8'h82; //6
+    4'b0111: Seven_Segment = 8'hF8; //7
+    4'b1000: Seven_Segment = 8'h80; //8
+    4'b1001: Seven_Segment = 8'h90; //9
+    endcase
+    end
 endmodule
