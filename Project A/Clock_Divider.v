d300cea8a65dbdd3aee2ffef53e151c8361ef164
AistoBasBistoC <ahhitsscott@gmail.com>
2021-04-15 17:39:13 -0700

    finished code for clock divider

--- Project A/Clock_Divider.v
+++ Project A/Clock_Divider.v
@@ -24,10 +24,18 @@ module Clock_Divider(
     input Clk, Reset,
     output Slow_Clock
     );
+    reg [25:0] Counter = 26'b0; //26 bit counter = 1 Hz clock speed
     
     //// write a Verilog code to slow down the clock from 100 MHz to 1 Hz
-
-  
+    
+    //Clock Divider
+    always @(posedge Clk, posedge Reset)
+        if(Reset)
+            Counter <= 26'b0;
+            else
+            Counter <= Counter + 1'b1;
+    
+    assign Slow_Clock = Counter[25];
     
     
 endmodule
